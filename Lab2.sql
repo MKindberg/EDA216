752c46e6a5ca21444c81ad93c2e2064937b8e0de
Marcus <marcus3111@gmail.com>
2016-02-03 21:20:42 +0100

    Tables created

--- Lab2.sql
+++ Lab2.sql
@@ -1,22 +1,48 @@
-CREATE TABLE user
+DROP TABLE IF EXISTS Users;
+DROP TABLE IF EXISTS Theatres;
+DROP TABLE IF EXISTS Movies;
+DROP TABLE IF EXISTS Performances;
+DROP TABLE IF EXISTS Reservations;
+
+CREATE TABLE Users
 (
-Username char(15),
-Name char(20) not null,
-Adress char(30),
-PhoneNbr char(11) not null,
-primary key(username)
+username char(15),
+name char(20) NOT NULL,
+adress char(30),
+phoneNbr char(11) NOT NULL,
+PRIMARY KEY(username)
 );
 
-CREATE TABLE Theatre
+CREATE TABLE Theatres
 (
-Name char(15),
-Seats int not null,
-primary key(name)
+name char(15),
+seats int NOT NULL,
+PRIMARY KEY(name)
 );
 
-CREATE TABLE Reservation
+CREATE TABLE Movies
 (
-RefNbr int,
-Username char(15),
+title char(20),
+PRIMARY KEY(title)
+);
 
-)
+CREATE TABLE Performances
+(
+title char(20),
+day date,
+freeSeats int NOT NULL,
+theatreName char(20)
+PRIMARY KEY(title, day),
+FOREIGN KEY(theatreName) REFERENCES Theatres(name)
+);
+
+CREATE TABLE Reservations
+(
+refNbr int AUTO_INCREMENT,
+username char(15),
+title char(20),
+day Date,
+PRIMARY KEY(refNbr),
+FOREIGN KEY(username) REFERENCES Users(username),
+foreign key(title, day) REFERENCES Performances(title, day)
+);
